49a4b7fb127c7555ef0c5bd169033c84283a0d15
drh <jmnabangi@gmail.com>
2022-07-17 13:54:04 +0300

    updates phone formatter to test invalid numbers separately
    Signed-off-by: drh <jmnabangi@gmail.com>

--- util/phone.go
+++ util/phone.go
@@ -3,16 +3,19 @@ package util
 import (
 	"errors"
 	"github.com/ttacon/libphonenumber"
+	"regexp"
 	"strings"
 )
 
 func GetPhoneByCountry(country string, phone string) (string, error) {
+	// TODO: Check out and substitute https://pkg.go.dev/github.com/nyaruka/phonenumbers
+
 	num, err := libphonenumber.Parse(phone, country)
 	if err != nil {
 		return phone, err
 	}
 
-	valid := libphonenumber.IsValidNumber(num)
+	valid := libphonenumber.IsValidNumber(num) || phoneValidKE(phone)
 	if !valid {
 		return phone, errors.New("number is not valid")
 	}
@@ -20,4 +23,13 @@ func GetPhoneByCountry(country string, phone string) (string, error) {
 	phone = strings.TrimPrefix(libphonenumber.Format(num, libphonenumber.E164), "+")
 
 	return phone, nil
+}
+
+func phoneValidKE(phone string) bool {
+	matchString, err := regexp.MatchString("/^(?:254|\\+254|0)?((?:7(?:[0129][0-9]|4[0123568]|5[789]|6[89])|(1([1][0-5])))[0-9]{6})$/", phone)
+	if err != nil {
+		return false
+	}
+
+	return matchString
 }
